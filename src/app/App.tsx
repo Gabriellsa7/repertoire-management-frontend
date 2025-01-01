50c8c6ba58f8cf8b3d92e676b5d0e279e8c0a02f
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2025-01-01 11:15:40 -0300

    feat: added more routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "../pages/login/login";
+import { Login } from "../pages/login/login";
 import { Signup } from "../pages/signup/signup";
 import { Home } from "../pages/Home/home";
 import { InitialPage } from "../pages/InitialPage/initial-Page";
+import { Profile } from "../pages/profile/profile";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
   );
