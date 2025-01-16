0205388c83215fc90cb7469818e1f2a0a1422560
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2025-01-16 09:06:48 -0300

    feat: changed fetch method

--- src/pages/Home/components/band-section.tsx
+++ src/pages/Home/components/band-section.tsx
@@ -18,7 +18,7 @@ export const BandSection = () => {
     }
 
     // Fetch bands associated with the user
-    fetch(`http://localhost:8080/user/${userId}/bands`, {
+    fetch(`http://localhost:8080/bands/leader/${userId}`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -46,18 +46,43 @@ export const BandSection = () => {
           </span>
         </button>
       </div>
-      <div className="flex gap-3 items-center">
+      {/* <div className="flex gap-3 items-center">
         {bands.length > 0
           ? bands.map((band) => (
               <div key={band.id} className="p-4 bg-gray-100 rounded-md">
-                <h3 className="text-lg font-bold">{band.name}</h3>
-                <p className="text-sm text-gray-600">{band.description}</p>
+                <h3 className="text-lg font-bold truncate">{band.name}</h3>
+                <p className="text-sm text-gray-600 truncate">
+                  {band.description}
+                </p>
               </div>
             ))
-          : // <button>
-            //   <img src="../../../assets/plus.png" alt="" />
-            // </button>
-            ""}
+          : ""}
+        <button>
+          <img src="../../../assets/plus.png" alt="" />
+        </button>
+      </div> */}
+      <div className="flex gap-3 items-center">
+        {bands.length > 0
+          ? bands.map((band) => (
+              <div className="flex flex-col gap-2">
+                <div
+                  key={band.id}
+                  className="flex justify-center items-center w-16 h-16 bg-gray-100 rounded-full overflow-hidden"
+                >
+                  <img
+                    src="../../../assets/profile.png"
+                    alt={band.name}
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+                {/* <div className="w-20">
+                  <h3 className="text-lg font-bold text-center truncate text-primary-text-color">
+                    {band.name}
+                  </h3>
+                </div> */}
+              </div>
+            ))
+          : ""}
         <button>
           <img src="../../../assets/plus.png" alt="" />
         </button>
